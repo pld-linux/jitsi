6e8413b464c00dfd0f08ae75c38ec4392f2d6112
Elan Ruusamäe <glen@pld-linux.org>
2011-05-11 20:04:09 +0000

    - startup script, based on .run installer produced one
    Changed files:
        jitsi.sh -> 1.2

--- jitsi.sh
+++ jitsi.sh
@@ -1,5 +1,30 @@
 #!/bin/sh
-exec java \
-	-Dfelix.config.properties=file:/usr/share/java/jitsi/lib/felix.client.run.properties \
-	-Djava.util.logging.config.file=/usr/share/java/jitsi/lib/logging.properties \
-	org.apache.felix.main.Main
+set -e
+
+# set JAVA_HOME from jpackage-utils if available
+if [ ! -f /usr/share/java-utils/java-functions ]; then
+	echo >&2 "jpackage-utils not found."
+	exit 1
+fi
+. /usr/share/java-utils/java-functions
+
+LIBDIR=/usr/lib/jitsi
+APPDIR=/usr/share/jitsi
+MAIN_CLASS=net.java.sip.communicator.launcher.SIPCommunicator
+
+for jar in $APPDIR/lib/*.jar; do
+	CLASSPATH=$CLASSPATH:$jar
+done
+for jar in sc-launcher.jar util.jar; do
+	CLASSPATH=$CLASSPATH:$APPDIR/sc-bundles/$jar
+done
+
+# extra JVM options
+OPTIONS="\
+	-Djna.library.path=$LIBDIR \
+	-Djava.library.path=$LIBDIR \
+	-Dfelix.config.properties=file:$APPDIR/lib/felix.client.run.properties \
+	-Djava.util.logging.config.file=$APPDIR/lib/logging.properties \
+"
+
+run "$@"
